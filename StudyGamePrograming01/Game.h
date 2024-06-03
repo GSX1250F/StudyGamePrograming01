ef8a3ad3d16796095d16732d60eeae0f124ae409
tanada@sandagakuen.ed.jp <tanada@sandagakuen.ed.jp>
2024-06-03 18:18:38 +0900

    update

--- StudyGamePrograming01/Game.h
+++ StudyGamePrograming01/Game.h
@@ -1,64 +1,35 @@
-// ----------------------------------------------------------------
-// From Game Programming in C++ by Sanjay Madhav
-// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
-// 
-// Released under the BSD License
-// See LICENSE in root directory for full details.
-// ----------------------------------------------------------------
-
 #pragma once
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
 #include "SDL.h"
 
-// Vector2 struct just stores x/y coordinates
-// (for now)
-// structはデフォルトのアクセシビリティが public
-// classはデフォルトのアクセシビリティが private
 struct Vector2
 {
-public: //←structだからあっても無くても動作は同じ。
 	float x;
 	float y;
 };
 
-// Game class
 class Game
 {
 public:
-	// コンストラクタ
-	// Initialize the game
 	Game();
-	// Runs the game loop until the game is over
 	bool Initialize();
 	void RunLoop();
-	// Shutdown the game
 	void Shutdown();
 private:
-	// Helper functions for the game loop
 	void ProcessInput();	// 1.入力を受け付ける
-	void UpdateGame();		// 2.ゲームを処理する
-	void GenerateOutput();	// 3.更新
+	void UpdateGame();		// 2.ゲームを処理・更新する
+	void GenerateOutput();	// 3.画面や音などを出力する
 
-	// Window created by SDL
-	SDL_Window* mWindow;
-	// Renderer for 2D drawing
-	SDL_Renderer* mRenderer;
-	// Number of ticks since start of game
-	Uint32 mTicksCount;
-	// Game should continue to run
-	bool mIsRunning;
+	SDL_Window* mWindow;	// SDLでつくられるウィンドウ
+	SDL_Renderer* mRenderer;	// 2D描画用レンダラ
+	Uint32 mTicksCount;		// ゲーム開始時からの経過時間
+	bool mIsRunning;		// ゲーム実行中か否か
 
-	// Pong specific
-	// Direction of paddle
-	int mPaddleDir;
-	// Position of paddle
-	Vector2 mPaddlePos;
-	// Position of Ball
-	Vector2 mBallPos;
-	// Velocity of Ball
-	Vector2 mBallVel;
-
-	
+	// Game specific
+	int mPaddleDir;		// パドルの動作方向。+が下方向、-が上方向。
+	Vector2 mPaddlePos;		// パドルの位置（2次元ベクトル形式）
+	Vector2 mBallPos;		// ボールの位置（2次元ベクトル形式）
+	Vector2 mBallVel;		// ボールの速度（2次元ベクトル形式）
 };
